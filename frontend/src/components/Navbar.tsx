691a74559ec6ad83995daf16ff1b524b4c489201
Gavin Hensley <gavinhensley@protonmail.com>
2025-02-14 17:21:21 -0500

    Updated styles for tailwind themes and navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -13,7 +13,7 @@ export default function Navbar() {
 
     return (
 
-        <nav className="fixed flex sm:justify-center left-0 p-4 top-0 w-full z-20">
+        <nav className="fixed flex sm:justify-center left-0 py-4 top-0 w-full z-20">
 
             <button id="nav-menu-btn" className={`sm:hidden absolute cursor-pointer flex flex-col h-10 items-end justify-around right-4 top-4 w-10 z-30 ${menuOpen ? "open-menu" : ""}`} onClick={toggleMenu}>
                 <div className="h-1 relative w-10 nav-menu-line"></div>
@@ -22,11 +22,18 @@ export default function Navbar() {
             </button>
 
             {/* Nav Menu Links */}
-            <ul id="nav-list" className="nav-list absolute flex flex-col gap-12 items-center justify-center sm:flex-row sm:top-4 top-24 w-full">
-                <li className="nav-li"><a href="#" className="nav-link">About</a></li>
-                <li className="nav-li"><a href="#" className="nav-link">Contact</a></li>
-                <li className="nav-li"><a href="#" className="nav-link">Home</a></li>
-                <li className="nav-li"><a href="#" className="nav-link">Projects</a></li>
+            <ul id="nav-list" className={`nav-list absolute bg-purple flex flex-col gap-12 
+                items-center justify-center p-8 sm:py-2 sm:px-4 sm:flex-row sm:top-4 h1-ff 
+                desktop-h1 top-24 text-mobile-body sm:text-desktop-body w-full sm:w-auto 
+                text-primary-cyan-900 font-body-ff duration-600 sm:translate-0 
+                ${menuOpen ? "" : "transform translate-x-full"} ${menuOpen ? "opacity-100" : "opacity-0"} 
+                sm:opacity-100`}>
+
+                <li className="nav-li"><a href="#" className="nav-link px-0.5">About</a></li>
+                <li className="nav-li"><a href="#" className="nav-link px-0.5">Contact</a></li>
+                <li className="nav-li"><a href="#" className="nav-link px-0.5">Home</a></li>
+                <li className="nav-li"><a href="#" className="nav-link px-0.5">Projects</a></li>
+
             </ul>
 
         </nav>
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,7 +1,8 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App'
-import './styles/globals.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import './styles/globals.css';
+import './styles/animations.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
