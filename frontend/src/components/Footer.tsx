7d10bafcf72a4feac71e6e59c556cc2afa189f96
Gavin Hensley <gavinhensley@protonmail.com>
2025-02-17 02:07:50 -0500

    Update footer and styles

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,5 +1,10 @@
 import './Footer.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faReact, faNode } from '@fortawesome/free-brands-svg-icons';
 import { GithubLogo, LinkedinLogo, XLogo } from '@phosphor-icons/react';
+import { BiLogoPostgresql, BiLogoTypescript, BiLogoTailwindCss, BiLogoHeroku, BiLogoGraphql } from "react-icons/bi";
+import { IoLogoVercel } from "react-icons/io5";
+import { SiPrisma } from "react-icons/si";
 
 export default function Footer() {
 
@@ -8,51 +13,99 @@ export default function Footer() {
 
     return (
 
-        <footer className='absolute bg-purple bottom-0 flex gap-32 justify-center py-6 text-primary-cyan-900 w-full'>
+        <footer className='absolute bg-purple bottom-0 flex flex-col md:flex-row gap-8 md:gap-12 lg:gap-32 py-6 pb-12 text-primary-cyan-900 w-full'>
 
-            <section id="contact-section">
+            <section id='tech-stack-section' className='flex flex-col gap-2 items-center mx-4 md:mx-0 md:ml-4 2xl:w-1/3'>
 
-                <h3 className="footer-title text-mobile-lg-body sm:text-desktop-lg-body">Contact Me</h3>
+                <h3 className="footer-title text-mobile-lg-body md:text-desktop-lg-body">Tech Stack</h3>
+                
+                <div className="icon-container flex gap-y-4 gap-x-8 flex-wrap 2xl:w-3/4 justify-center">
+                    <a target='_blank' href="https://react.dev/" className="icon-link h-fit">
+                        <FontAwesomeIcon className='icon text-4xl md:text-5xl' icon={faReact} />
+                    </a>
 
-                <form className='flex flex-col items-center mb-4'>
-                    <div id="name-email-container" className='flex gap-4'>
+                    <a target='_blank' href="https://react.dev/" className="icon-link h-fit">
+                        <FontAwesomeIcon className='icon text-4xl md:text-5xl' icon={faNode} />
+                    </a>
+
+                    <a target='_blank' href="https://react.dev/" className="icon-link h-fit">
+                        <BiLogoPostgresql className='icon text-4xl md:text-5xl'/>
+                    </a>
+
+                    <a target='_blank' href="https://react.dev/" className="icon-link h-fit">
+                        <SiPrisma  className='icon text-4xl md:text-5xl'/>
+                    </a>
+
+                    <a target='_blank' href="https://react.dev/" className="icon-link h-fit">
+                        <BiLogoTypescript className='icon text-4xl md:text-5xl'/>
+                    </a>
+
+                    <a target='_blank' href="https://react.dev/" className="icon-link h-fit">
+                        <BiLogoTailwindCss className='icon text-4xl md:text-5xl'/>
+                    </a>
+
+                    <a target='_blank' href="https://react.dev/" className="icon-link h-fit">
+                        <BiLogoGraphql className='icon text-4xl md:text-5xl'/>
+                    </a>
+
+                    <a target='_blank' href="https://react.dev/" className="icon-link h-fit">
+                        <BiLogoHeroku className='icon text-4xl md:text-5xl'/>
+                    </a>
+
+                    <a target='_blank' href="https://react.dev/" className="icon-link h-fit">
+                        <IoLogoVercel className='icon text-4xl md:text-5xl'/>
+                    </a>
+                </div>
+
+            </section>
+
+            <section id="contact-section" className='self-center h-fit w-fit md:justify-bottom'>
+
+                <h3 className="footer-title text-mobile-lg-body md:text-desktop-lg-body">Contact Me</h3>
+
+                <form className='flex flex-col items-center md:mb-4'>
+                    <div id="name-email-container" className='flex flex-col xl:flex-row xl:gap-4 w-full'>
                         <div className="input-container flex flex-col">
-                            <label htmlFor="name" className="font-body-ff">Name:</label>
+                            <label htmlFor="name" className="font-body-ff md:text-desktop-body">Name:</label>
                             <input type="text" id="name" className="contact-input" placeholder='Your Name' required/>
                         </div>
 
                         <div className="input-container flex flex-col">
-                            <label htmlFor="email" className="font-body-ff">Email:</label>
-                            <input type="email" id="email" className="contact-input" placeholder='Email' required/>
+                            <label htmlFor="email" className="font-body-ff md:text-desktop-body">Email:</label>
+                            <input type="email" id="email" className="contact-input" placeholder='Your Email' required/>
                         </div>
                     </div>
-                    <div className="message-btn-container flex gap-12 items-center">
-                        <div className="input-container flex flex-col">
-                            <label htmlFor="message" className="font-body-ff">Message:</label>
-                            <textarea id="message" className="contact-input" placeholder='Message' rows={4} required/>
+                    <div className="message-btn-container flex flex-col gap-4 items-center w-full">
+                        <div className="input-container flex flex-col w-full">
+                            <label htmlFor="message" className="font-body-ff md:text-desktop-body">Message:</label>
+                            <textarea id="contact-message" className="contact-input" placeholder='Your Message Here...' rows={4} required/>
                         </div>
 
-                        <button id='contact-btn' type="submit" className="btn py-2 px-4 font-body-ff h-fit mt-2">Submit</button>
+                        <button id='contact-btn' type="submit" className="btn py-2 px-4 font-body-ff h-fit md:text-desktop-body">SUBMIT</button>
                     </div>
                 </form>
 
             </section>
 
-            <section id="social-section" className='flex flex-col gap-4 h-fit py-2'>
+            <section id="social-section" className='flex flex-col gap-2 justify-center items-center md:mr-4 2xl:w-1/3 h-full'>
 
-                <a target='_blank' href="https://github.com/G-Hensley/" className="social-link h-fit">
-                    <GithubLogo size={36} className='social-icon'/>
-                </a>
-                <a target='_blank' href="https://linkedin.com/in/g-hensley/" className="social-link h-fit">
-                    <LinkedinLogo size={36} className='social-icon'/>
-                </a>
-                <a target='_blank' href="https://x.com/GavinHensleyDev" className="social-link h-fit">
-                    <XLogo size={36} className='social-icon'/>
-                </a>
+                <h3 className="footer-title text-mobile-lg-body md:text-desktop-lg-body">Social</h3>
+
+                <div className="icon-container flex md:flex-col gap-y-4 gap-x-8 items-center 2xl:w-3/4">
+                    <a target='_blank' href="https://github.com/G-Hensley/" className="icon-link h-fit">
+                        <GithubLogo className='icon text-4xl md:text-5xl'/>
+                    </a>
+                    <a target='_blank' href="https://linkedin.com/in/g-hensley/" className="icon-link h-fit">
+                        <LinkedinLogo className='icon text-4xl md:text-5xl'/>
+                    </a>
+                    <a target='_blank' href="https://x.com/GavinHensleyDev" className="icon-link h-fit">
+                        <XLogo className='icon text-4xl md:text-5xl'/>
+                    </a>
+                </div>
 
             </section>
 
-            <p id='copyright' className='absolute bottom-0 text-md text-primary-magenta-900 font-body-ff'>
+            <p id='copyright' className='absolute bottom-0 text-md text-primary-magenta-900 font-body-ff w-full text-center bg-charcoal py-2'>
                 © {year} Gavin Hensley. All Rights Reserved.
             </p>
 
